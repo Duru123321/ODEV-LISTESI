b8c286224695a8943f203f10644efd2a7c44cf30
Duru123321 <durucaltekin018@gmail.com>
2025-10-25 19:33:59 +0300

    odev uc

---
+++ Oodevucuncu.java
@@ -0,0 +1,12 @@
+public class Oodevucuncu {
+    static void main(String[] args) {
+        String yazi1="ISMINIZ NEDIR";
+        System.out.println(yazi1.toLowerCase());
+        String yazi2=("isminiz nedir");
+        System.out.println(yazi2.toUpperCase());
+        String yazi3="kırmızı rengi severim";
+        System.out.println(yazi3.replace("kırmızı","yeşil"));
+        String yazi4="Bahçelievler Anadolu Lisesi";
+        System.out.println(yazi4.contains("Anadolu"));
+    }
+}
