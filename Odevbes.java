98090b16e5d6856b4f040ad44356490f89db6715
Duru123321 <durucaltekin018@gmail.com>
2025-11-14 23:03:40 +0300

    Add files via upload

--- Odevbes.java
+++ Odevbes.java
@@ -2,13 +2,22 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Odevbes {
-    static void main(String[] args) {
-        int[] sayilar=new int[50];
-        Random r= new Random();
-        for (int i= 0; i<50;i++)
-sayilar[i]= r.nextInt(1000)+1;
-        Arrays.sort(sayilar);
-        for(int s:sayilar)
-System.out.println(s+"");
+    public static void main(String[] args) {
+
+        int[] n = new int[50];
+        Random r = new Random();
+
+        for (int i = 0; i < 50; i++) n[i] = r.nextInt(101);
+
+        for (int i = 0; i < 49; i++)
+            for (int j = 0; j < 49 - i; j++)
+                if (n[j] > n[j + 1]) {
+                    int t = n[j];
+                    n[j] = n[j + 1];
+                    n[j + 1] = t;
+                }
+
+        for (int x : n) System.out.print(x + " ");
     }
+
 }
