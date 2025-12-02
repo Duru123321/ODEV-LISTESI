4c39970a579d390730ebca66011f833669f33bfd
Duru123321 <durucaltekin018@gmail.com>
2025-12-02 19:57:13 +0300

    Add files via upload

---
+++ Day.java
@@ -0,0 +1,21 @@
+public class Day {
+    ders ders1;
+    ders ders2;
+    ders ders3;
+    ders ders4;
+    ders ders5;
+    ders ders6;
+    ders ders7;
+    ders ders8;
+    public Day(String ingilizce, String s, String string, String edebiyat){
+        this.ders1=ders1;
+        this.ders2=ders2;
+        this.ders3=ders3;
+        this.ders4=ders4;
+        this.ders5=ders5;
+        this.ders6=ders6;
+        this.ders7=ders7;
+        this.ders8=ders8;
+
+    }
+}
