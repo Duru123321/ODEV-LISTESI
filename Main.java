dafcd3f9ae211564762fffc98f896eb576150f89
Duru123321 <durucaltekin018@gmail.com>
2025-12-02 19:56:52 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    ders matematik= new ders("Murat Aslan","ders kitabı ve fotokopi","80","100");
+    ders ingilizce= new ders("Erman Çekinmez,Hülya tekin,Nermin Özbek"," ","85","100");
+    ders edebiyat= new ders("Mustafa Tekin","aylık kitap okuma","87","90");
+    ders beden= new ders("yılmaz yavuz","","100","100");
+    ders yazılım= new ders("özgür demir"," ","100","100");
+    ders resim= new ders("aysel çalışkan"," ","90","100");
+    ders almanca= new ders("şeyma eser büyükdere","ders kitabı ve calısma kitabı","83","100");
+    Day pazt= new Day("ingilizce", "ingilizce", "ingilizce", "ingilizce");
+    Day sali= new Day("ingilizce", "ingilizce", "ingilizce", "ingilizce");
+    Day carsamba=new Day("ingilizce", "ingilizce", "almanca", "almanca");
+    Day persembe=new Day("ingilizce", "ingilizce", "ingilizce", "matematik");
+    Day cuma=new Day("ingilizce","ingilizce","edebiyat","edebiyat");
+}
