bc1330c15e56949e9585bcc5b8aec4640fb3843b
Duru123321 <durucaltekin018@gmail.com>
2025-12-02 19:58:53 +0300

    Add files via upload

---
+++ ders.java
@@ -0,0 +1,13 @@
+public class ders {
+    String teacher;
+    String homework;
+    int examMark;
+    int quizMark;
+    public ders(String teacher, String homework, String examMark, String quizMark){
+        this.teacher= teacher;
+        this.homework= homework;
+        this.examMark= examMark;
+        this.quizMark= quizMark;
+  }
+}
+
